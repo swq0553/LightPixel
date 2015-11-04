48881d1e23af1f7267bbdfcfe79932a554f53368
swq0553 <swq0553@sina.com>
2015-11-04 16:28:37 +0800

    first version

---
+++ Next/Next.cpp
@@ -0,0 +1,1 @@
+#include<Next.hpp>
---
+++ Next/Next.hpp
@@ -0,0 +1,20 @@
+#ifndef _NEXT_HPP
+#define _NEXT_HPP
+
+#define NEXT_NAME			NEXT
+#define NEXT_VER_MAJOR	1
+#define NEXT_VER_MINOR	0
+#pragma once
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <assert.h>
+
+#include <GL/glew.h>
+#include <GL/wglew.h>
+
+#include <PreDeclare.hpp>
+#include <Engine.hpp>
+
+#endif		// _NEXT_HPP
